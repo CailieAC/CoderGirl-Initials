389e32b935fe0a0782f526e8a42e0ed047f3fc93
Carlile Upfront <carlileupfront@users.noreply.github.com>
2019-03-09 19:05:20 -0600

    commit1

--- Initials/Program.cs
+++ Initials/Program.cs
@@ -6,9 +6,27 @@ namespace Initials
     {
         public static void Main(string[] args)
         {
+            Console.Write("Enter a name: ");
+            string name = Console.ReadLine();
+            string initials = Initials(name);
+            Console.WriteLine(initials);
             Console.ReadLine();
         }
 
         // TODO: Create a method that takes in a full name and return the initials in all capital letters.
+        public static string Initials(string name)
+        {
+            string initials = "";
+            string[] arrayOfName = name.Split(' ');
+            char[] arrayOfInitials = new char[arrayOfName.Length];
+            for (int i = 0; i < arrayOfName.Length; i++)
+            {
+                arrayOfName[i] = arrayOfName[i].ToUpper().Trim();
+                arrayOfInitials[i] = arrayOfName[i][0];
+            }
+
+            initials = String.Join("", arrayOfInitials);
+            return initials;
+        }
     }
 }
